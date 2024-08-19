11b01937013f819272864cfa127d01f7d26eb4b0
LOgiclyric1432 <dheerajsharma16063@gmail.com>
2024-08-18 21:54:01 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function openLightbox(element) {
+    const lightbox = document.getElementById('lightbox');
+    const lightboxImg = document.getElementById('lightbox-img');
+
+    lightboxImg.src = element.querySelector('img').src;
+    lightbox.style.display = 'flex';
+}
+
+function closeLightbox() {
+    const lightbox = document.getElementById('lightbox');
+    lightbox.style.display = 'none';
+}
